0c091b0dea3a993144e930f11e81126ce2bd5345
Lyddem <lydiademi@gmail.com>
2017-09-08 11:34:17 -0700

    Completed

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(){
+    super();
+
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = [];
+    this.forage = function(treasure) {
+      this.treasureChest.push(treasure)
+    };
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    this.makeHoney = function() {
+      this.honeyPot++;
+    };
+    this.giveHoney = function() {
+      this.honeyPot--;
+    };
+
+  }
 };
